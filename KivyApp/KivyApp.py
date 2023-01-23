c0f6ffabd51196d8ffc47f250bb00b4f69c239fa
Mchelix28MK <michele.folci04@gmail.com>
2023-01-24 00:40:17 +0100

    kivy

--- KivyApp/KivyApp.py
+++ KivyApp/KivyApp.py
@@ -5,6 +5,7 @@ from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.network.urlrequest import UrlRequest
 class TrovaTemperatura(App) :
     #funzione utilizzata per costruire la struttura del'app
     def build(self) :
@@ -35,23 +36,27 @@ class TrovaTemperatura(App) :
                             bold = True,
                             background_color = '#0099ff'
                             )
+        self.bottone.bind(on_press = self.trova_tempo)
         self.testo_output = Label(
                                 text="Cerca una città ...",
                                 font_size= '20sp',
                                 color = '#007dd1'
                                 )
+        self.Api_Key = "581babd915a3c3b35cddb01cf9f876bf"
         #widget
         self.window.add_widget(self.testo_input)
         self.window.add_widget(self.bottone)
         self.window.add_widget(self.testo_output)
-        #logica
-        self.bottone.bind(on_press = self.trova_temp)
-        
         return self.window
     
+        #logica        
+    
     def trova_tempo(self, instance) :
-        self.testo_output.text = f"Oggi a {self.testo_input.text} Ci saranno " 
-        #la 'f' posta prima della stringa la rende formattata
+        link = f"https://api.openweathermap.org/data/2.5/weather?q={self.testo_input}&appid={self.Api_Key}&units=metric"
+        def edit_lable(request, result):
+            temp = result['main']['temp'] #estrazione del valore presente dentro MAIN(TEMP(x))
+            self.testo_output.text = f"Oggi a {self.testo_input.text} Ci saranno {temp}°C" #la 'f' posta prima della stringa la rende formattata
+        UrlRequest(link, edit_lable)
         
 #Lancio dell'applicazione
 TrovaTemperatura().run()
